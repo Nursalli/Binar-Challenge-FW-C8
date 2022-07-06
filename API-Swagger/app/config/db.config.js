aba855f24db164f95050c88b7299e5b2e331b4c9
Muhammad Nursalli <shallimuhammad@gmail.com>
2022-07-06 09:30:28 +0700

    Init Project

---
+++ API-Swagger/app/config/db.config.js
@@ -0,0 +1,14 @@
+// setup database here, change the values to suit your environment
+module.exports = {
+  HOST: "127.0.0.1",
+  USER: "nursalli",
+  PASSWORD: "nursalli123",
+  DB: "db_challenge_fw_c8",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
